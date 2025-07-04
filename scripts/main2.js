67962ebff33cd2872c4b855ed4ad919bc70d964a
nshfdij <728765865@qq.com>
2025-07-04 19:45:51 +0800

    修改一些内容

--- scripts/main2.js
+++ scripts/main2.js
@@ -9,12 +9,26 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "images/firefox-icon.png");
   }
 };
-
+//点击按钮，出现prompt对话框，输入名字，保存到localStorage中
+//如果localStorage中有名字，则显示“Mozilla is cool, xxx”
+//没有名字，则显示“Mozilla is cool, (输入的名字)”，并将名字保存到localStorage中
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
 myButton.onclick = () => {
-  const userName = prompt("请输入你的名字。");
-  localStorage.setItem("name", userName);
-  myHeading.textContent = `Mozilla is cool,${userName}！`;
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
 };
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
