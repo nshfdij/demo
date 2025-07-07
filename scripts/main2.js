70727cb9de5742dcd320ef96720f8cc70983d907
xu <14543657+ab11111111111111@user.noreply.gitee.com>
2025-07-07 12:50:59 +0800

    feat: 新增多个 HTML 页面和样式 - 新增 challenge1_email.html 页面，用于展示电子邮件模板 - 新增 email.css 样式文件，用于 challenge1_email.html 的样式 - 修改 index.html 页面，添加更多内容和结构 - 更新 main2.js 脚本，优化图像切换逻辑 - 新增 thing.html 页面，用于测试事件触发 - 更新 style.css 样式文件，增加图片和描述列表的样式

--- scripts/main2.js
+++ scripts/main2.js
@@ -3,10 +3,10 @@ const myImage = document.querySelector("img");
 //getAttribute()与setAttribute()方法
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/firefox2.png");
+  if (mySrc === "images/火狐.png") {
+    myImage.setAttribute("src", "images/firefox-icon.jpg");
   } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
+    myImage.setAttribute("src", "images/火狐.png");
   }
 };
 //点击按钮，出现prompt对话框，输入名字，保存到localStorage中
@@ -14,21 +14,17 @@ myImage.onclick = () => {
 //没有名字，则显示“Mozilla is cool, (输入的名字)”，并将名字保存到localStorage中
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
-
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
 myButton.onclick = () => {
-  if (!localStorage.getItem("name")) {
-    setUserName();
-  } else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Mozilla is cool, ${storedName}`;
-  }
+  setUserName();
 };
 function setUserName() {
   const myName = prompt("Please enter your name.");
-  if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
-  }
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
 }
